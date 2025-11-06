14501db85f2bf484506e7b59eca688c733821ec3
Erika Mayara <erikamayara088@gmail.com>
2025-11-06 16:18:25 -0300

    adiciona classe Atleta com métodos para cálculo de categoria, IMC e média válida

---
+++ dadosAtletas.js
@@ -0,0 +1,71 @@
+class Atleta {
+    constructor(nome, idade, peso, altura, notas) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+        this.notas = notas;
+    }
+
+    calculaCategoria() {
+        if (this.idade >= 9 && this.idade <= 11) {
+            return "Infantil";
+        } else if (this.idade >= 12 && this.idade <= 13) {
+            return "Juvenil";
+        } else if (this.idade >= 14 && this.idade <= 15) {
+            return "Intermediário";
+        } else if (this.idade >= 16 && this.idade <= 30) {
+            return "Adulto";
+        } else {
+            return "Sem categoria";
+        }
+    }
+
+    calculaIMC() {
+        return this.peso / (this.altura * this.altura);
+    }
+
+    calculaMediaValida() {
+        let notasOrdenadas = this.notas.slice().sort((a, b) => a - b);
+        notasOrdenadas.shift(); 
+        notasOrdenadas.pop();  
+
+        let soma = notasOrdenadas.reduce((acc, atual) => acc + atual, 0);
+        return soma / notasOrdenadas.length;
+    }
+
+    obtemNomeAtleta() {
+        return this.nome;
+    }
+
+    obtemIdadeAtleta() {
+        return this.idade;
+    }
+
+    obtemPesoAtleta() {
+        return this.peso;
+    }
+
+    obtemNotasAtleta() {
+        return this.notas;
+    }
+
+    obtemCategoria() {
+        return this.calculaCategoria();
+    }
+
+    obtemIMC() {
+        return this.calculaIMC();
+    }
+
+    obtemMediaValida() {
+        return this.calculaMediaValida();
+    }
+}
+const atleta = new Atleta("Amanda", 14, 60, 1.7, [9, 8, 10, 7, 9]);
+console.log(atleta.obtemNomeAtleta()); 
+console.log(atleta.obtemCategoria()); 
+console.log(atleta.obtemIMC().toFixed(2));
+console.log(atleta.obtemMediaValida().toFixed(2)); 
+
+
